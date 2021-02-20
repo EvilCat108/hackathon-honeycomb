9e89c81f7442de9bb54acf6fb50924b4dfb1832e
vladislavs.poznaks <vladislavs.poznaks@accenture.com>
2021-02-20 16:01:57 +0200

    * Add link action

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,13 +1,22 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
-	public String showPage() {
+	@GetMapping("/listresult")
+	public String showSecondPage() {
+		return "listresult";
+	}
+
+
+	@GetMapping("/")
+	public String showFirstPage() {
 		return "hotelreservation";
 	}
+
+
 }
